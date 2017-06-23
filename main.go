4c028b06d9b7528b597d7a4298530ddb6046d126
millidavids <david.a.yurek@gmail.com>
2017-06-23 01:03:33 -0400

    successfully grabbing my stats

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
-// Copyright 2015 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
-
-// Sample helloworld is a basic App Engine flexible app.
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+  "os"
+  "io/ioutil"
 )
 
 func main() {
-	http.HandleFunc("/", handle)
+  http.HandleFunc("/", handle)
 	http.HandleFunc("/_ah/health", healthCheckHandler)
 	log.Print("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -23,7 +20,14 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello world!")
+
+  client := &http.Client{}
+  req, _ := http.NewRequest("GET", "https://pubgtracker.com/api/profile/pc/millidavids", nil)
+  req.Header.Add("TRN-API-KEY", os.Getenv("TRN_API_KEY"))
+  resp, _ := client.Do(req)
+  body, _ := ioutil.ReadAll(resp.Body)
+  bodyString := string(body)
+	fmt.Fprint(w, bodyString)
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
